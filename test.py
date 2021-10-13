2742fc761139f6d4c8ec90d08c84345180ff6a35
bliz4rd <thibautbranlant@gmail.com>
2021-10-13 14:43:40 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+#x= 10.2 
+#x1 = 10,2
+#x2 = "10,2"
+#x2_v2 = list(x2)
+#print(type(x2_v2))
+#x2_v2[2] = "."
+#print(type(x2_v2))
+#print(x2_v2)
+#x2_v3 = ''.join(x2_v2)
+#print(x2_v3)
+#print(type(x2_v3))
+#print(type(float(x2_v3)))
+#print(type(x2_v3))
